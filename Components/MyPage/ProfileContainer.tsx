19ace2e4ffdddadb61d8edfb272879c24690cca4
nno3onn <nno3onn@naver.com>
2023-02-10 22:46:41 +0900

    fix: 피드백 반영

--- Components/MyPage/ProfileContainer.tsx
+++ Components/MyPage/ProfileContainer.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const ProfileContainer = styled.div`
-  padding: 52px;
+  padding: 3.25rem;
   background-color: #f2f2f2;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 8px;
+  box-shadow: 0rem 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
+  border-radius: 0.5rem;
 `;
 
 export default ProfileContainer;
