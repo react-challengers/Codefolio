8316920d4adece93776377e47b9155d67be58e1b
Yunjunho <yunjunhojj@gmail.com>
2023-02-10 22:49:39 +0900

    feat: 쓸데없는 코드 삭제 #6

--- Components/Common/AuthLoginButton.tsx
+++ Components/Common/AuthLoginButton.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const LoginButton = styled.button`
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
   margin: 0.625rem;
@@ -10,20 +9,12 @@ const LoginButton = styled.button`
   width: 27.1875rem;
   height: 3.6875rem;
 
-  border: 0.0625rem solid #333333;
+  border: 1px solid #333333;
   border-radius: 6.25rem;
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
 
   font-family: "Noto Sans KR";
   font-style: normal;
   font-size: 1.125rem;
-
-  display: flex;
-  align-items: center;
-  text-align: center;
 
   cursor: pointer;
 
