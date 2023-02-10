15e951d50e1b845b94ced3c1f91afa3384e6b0bc
arch-spatula <shkim516@naver.com>
2023-02-10 17:25:07 +0900

    refactor: Tags.tsx Props type 식별자명 수정
    - TagsItems를 tagItems로 복수형의 복수형을 단수형의 복수형으로 수정합니다.
        - 복수의 목록이 아닌 단일 목록이기 때문에 변경합니다.
        - 파스칼 케이스를 케멀케이스로 수정합니다.
    
    #8

--- Components/Common/Tags.tsx
+++ Components/Common/Tags.tsx
@@ -2,22 +2,26 @@ import styled from "styled-components";
 
 /**
  * @TODO Set 자료형으로 리팩토링하기. 중복을 막아야 합니다.
- * Set에는 순회하는 메서드를 찾아야 합니다.
- * 메서드 찾아주시면 PR에 추가 커밋으로 반영하겠습니다.
+ * 1. Set에는 순회하는 메서드를 찾아야 합니다.
+ * 2. 메서드 찾아주시면 PR에 추가 커밋으로 반영하겠습니다.
+ *
+ * @TODO size: "sm" | "lg" | "md"; 추가하기
+ * 동작하는 로직을 추가할 때는 여기로 추가합니다.
+ * 아이콘 클릭에 삭제, 태그 클릭에 추가...
  */
 
 interface TagsProps {
-  TagsItems: string[];
+  tagItems: string[];
 }
 
 /**
  * 문자열만 들어간 Array만 Props로 받습니다.
  * li태그를 styled 컴포넌트로 활용하지 않은 이유는 key가 없기 때문입니다.
  */
-const Tags = ({ TagsItems }: TagsProps) => {
+const Tags = ({ tagItems }: TagsProps) => {
   return (
     <TagsContainer>
-      {TagsItems.map((item) => (
+      {tagItems.map((item) => (
         <TagsItem key={item}>{item}</TagsItem>
       ))}
     </TagsContainer>
