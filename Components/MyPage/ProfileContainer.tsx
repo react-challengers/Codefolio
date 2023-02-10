5864c3431f40e346bbba300106cf29f7dc467ee7
nno3onn <nno3onn@naver.com>
2023-02-10 20:41:03 +0900

    feat(ProfileContainer): MyPage의 ProfileContainer 컴포넌트 구현

---
+++ Components/MyPage/ProfileContainer.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ProfileContainer = styled.div`
+  padding: 52px;
+  background-color: #f2f2f2;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+`;
+
+export default ProfileContainer;
