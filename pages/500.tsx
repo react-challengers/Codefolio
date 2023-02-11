f01c21c7473d8c24e7c719daa022795166ff2097
arch-spatula <shkim516@naver.com>
2023-02-11 09:37:31 +0900

    refactor (404, 500): GNB만큼 높이 줄이기
    - GNB만큼 높이를 작게 조정합니다.
    
    #32

--- pages/500.tsx
+++ pages/500.tsx
@@ -33,7 +33,7 @@ const PageNotFoundContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: calc(100vh - 3.5rem);
 `;
 
 const Ellipse = styled.div`
