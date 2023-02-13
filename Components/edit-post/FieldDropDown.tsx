2ed222d31f22f0c7d6f64e53d972b4c640bde184
arch-spatula <shkim516@naver.com>
2023-02-13 12:05:53 +0900

    feat(FieldDropDown): 하드코딩 컴포넌트 구현
    - 임시 커밋으로 저장합니다.
    
    #36

--- Components/edit-post/FieldDropDown.tsx
+++ Components/edit-post/FieldDropDown.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 /**
  * @TODO Tabs 로직 참조하기
  */
 
-const field = [
+type LargeCategoryType =
+  | "웹"
+  | "앱"
+  | "소프트웨어"
+  | "데이터"
+  | "블록체인"
+  | "데브옵스"
+  | "IoT/임베디드"
+  | "보안";
+
+const field: LargeCategoryType[] = [
   "웹",
   "앱",
   "소프트웨어",
@@ -15,23 +26,97 @@ const field = [
   "보안",
 ];
 
+enum Field {
+  WEB = "웹",
+  APP = "앱",
+  SOFTWARE = "소프트웨어",
+  DATA = "데이터",
+  WEB3 = "블록체인",
+  DEVOPS = "데브옵스",
+  IOT_AND_EMBEDDED = "IOT,임베디드",
+  SECURITY = "보안",
+}
+
+const FiledItemContainer = styled.li<{
+  key?: string;
+  onMouseOver?: () => void;
+}>`
+  padding: 0.75rem;
+  width: 8rem;
+  :hover {
+    background: #e6e6e6;
+  }
+`;
+
+const lookup: { [key: string]: JSX.Element } = {
+  WEB: (
+    <ul>
+      <FiledItemContainer>웹</FiledItemContainer>
+    </ul>
+  ),
+  APP: (
+    <ul>
+      <FiledItemContainer>앱</FiledItemContainer>
+    </ul>
+  ),
+  SOFTWARE: (
+    <ul>
+      <FiledItemContainer>사무자동화</FiledItemContainer>
+      <FiledItemContainer>공장자동화</FiledItemContainer>
+      <FiledItemContainer>ERP</FiledItemContainer>
+      <FiledItemContainer>유니티</FiledItemContainer>
+      <FiledItemContainer>언리얼</FiledItemContainer>
+      <FiledItemContainer>기타</FiledItemContainer>
+    </ul>
+  ),
+  DATA: (
+    <ul>
+      <FiledItemContainer>데이터</FiledItemContainer>
+    </ul>
+  ),
+  WEB3: (
+    <ul>
+      <FiledItemContainer>블록체인</FiledItemContainer>
+    </ul>
+  ),
+  DEVOPS: (
+    <ul>
+      <FiledItemContainer>데브옵스</FiledItemContainer>
+    </ul>
+  ),
+  IOT_AND_EMBEDDED: (
+    <ul>
+      <FiledItemContainer>IOT,임베디드</FiledItemContainer>
+    </ul>
+  ),
+  SECURITY: (
+    <ul>
+      <FiledItemContainer>보안</FiledItemContainer>
+    </ul>
+  ),
+};
+
 const FieldDropDown = () => {
+  const [largeCategory, setLargeCategory] = useState<string>("웹");
+  const handleLargeCategory = (item: LargeCategoryType) => {
+    setLargeCategory(item);
+  };
+
   return (
     <FieldDropDownContainer>
+      <h1>{Field.APP}</h1>
       <ul>
         {field.map((item) => (
-          <FiledItemContainer key={item}>{item}</FiledItemContainer>
+          <FiledItemContainer
+            onMouseOver={() => handleLargeCategory(item)}
+            key={item}
+          >
+            {item}
+          </FiledItemContainer>
         ))}
       </ul>
       <Divider />
-      <ul>
-        <FiledItemContainer>사무자동화</FiledItemContainer>
-        <FiledItemContainer>공장자동화</FiledItemContainer>
-        <FiledItemContainer>ERP</FiledItemContainer>
-        <FiledItemContainer>유니티</FiledItemContainer>
-        <FiledItemContainer>언리얼</FiledItemContainer>
-        <FiledItemContainer>기타</FiledItemContainer>
-      </ul>
+      {lookup.WEB}
     </FieldDropDownContainer>
   );
 };
@@ -48,14 +133,6 @@ const FieldDropDownContainer = styled.div`
 const Divider = styled.hr`
   padding: 0;
   margin: 0;
-`;
-
-const FiledItemContainer = styled.li<{ key?: string }>`
-  padding: 0.75rem;
-  width: 8rem;
-  :hover {
-    background: #e6e6e6;
-  }
 `;
 
 export default FieldDropDown;
