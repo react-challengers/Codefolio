8e0bb1fa21bdefe0d9bb2e21f046a242bfe21ce8
arch-spatula <shkim516@naver.com>
2023-02-14 16:03:17 +0900

    refactor(FieldDropDown): types.d.ts로 타입 지정
    - types.d.ts의 타입을 위주로 지정합니다.
    
    #5 #36

--- Components/edit-post/FieldDropDown.tsx
+++ Components/edit-post/FieldDropDown.tsx
@@ -2,41 +2,31 @@ import { useReducer } from "react";
 import styled from "styled-components";
 
 /**
- * reducer함수의 state인자는 다음 action인자를 작성하기 위해 존재합니다.
- * @TODO 하위 분야는 cursor pointer css를 조건부로 추가합니다.
- * @TODO visibility는 호출한 컴포넌트에서 제어합니다.
- * @TODO 하위 분야 state 제어는 recoil로 처리합니다.
+ * reducer함수의 _state매개변수는 다음 action인자를 작성하기 위해 존재합니다.
  * @example
  * <FieldDropDown />
  */
 
-type LargeCategoryType =
-  | "웹"
-  | "앱"
-  | "소프트웨어"
-  | "데이터"
-  | "블록체인"
-  | "데브옵스"
-  | "IoT/임베디드"
-  | "보안";
+/**
+ * 아래 정의가 없으면 "ReferenceError: Field is not defined"에러가 발생합니다.
+ * enum의 데이터는 전역으로 공유되지 않는 것 같습니다. 타입은 전역으로 공유됩니다.
+ */
+enum Field {
+  WEB = "웹",
+  APP = "앱",
+  SOFTWARE = "소프트웨어",
+  DATA = "데이터",
+  WEB3 = "블록체인",
+  DEVOPS = "데브옵스",
+  IOT_AND_EMBEDDED = "IOT,임베디드",
+  SECURITY = "보안",
+}
 
-const field: LargeCategoryType[] = [
-  "웹",
-  "앱",
-  "소프트웨어",
-  "데이터",
-  "블록체인",
-  "데브옵스",
-  "IoT/임베디드",
-  "보안",
-];
+const field = Object.values(Field);
 
 const initialState: string[] = ["프론트엔드", "백엔드", "풀스택"];
 
-const reducer = (
-  _state: string[],
-  action: { type: LargeCategoryType }
-): string[] => {
+const reducer = (_state: string[], action: { type: Field }): string[] => {
   switch (action.type) {
     case "웹":
       return ["프론트엔드", "백엔드", "풀스택"];
@@ -50,7 +40,7 @@ const reducer = (
       return ["블록체인"];
     case "데브옵스":
       return ["데브옵스"];
-    case "IoT/임베디드":
+    case "IOT,임베디드":
       return ["IOT,임베디드"];
     case "보안":
       return ["보안"];
@@ -61,7 +51,7 @@ const reducer = (
 
 const FieldDropDown = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const handleLargeCategory = (item: LargeCategoryType) => {
+  const handleLargeCategory = (item: Field) => {
     dispatch({ type: item });
   };
 
@@ -80,7 +70,9 @@ const FieldDropDown = () => {
       <Divider />
       <ul>
         {state.map((item) => (
-          <FiledItemContainer key={item.toString()}>{item}</FiledItemContainer>
+          <FiledItemContainer key={item.toString()} cursor>
+            {item}
+          </FiledItemContainer>
         ))}
       </ul>
     </FieldDropDownContainer>
@@ -90,9 +82,11 @@ const FieldDropDown = () => {
 const FiledItemContainer = styled.li<{
   key?: string;
   onMouseOver?: () => void;
+  cursor?: boolean;
 }>`
   padding: 0.75rem;
   width: 8.75rem;
+  ${(props) => props.cursor && "cursor: pointer;"};
   :hover {
     background: #e6e6e6;
   }
@@ -100,16 +94,17 @@ const FiledItemContainer = styled.li<{
 
 const FieldDropDownContainer = styled.div`
   width: 21.5rem;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
   border: 1px solid #cccccc;
   display: flex;
-  border-radius: 4px;
+  border-radius: 0.2rem;
   gap: 0;
 `;
 
 const Divider = styled.hr`
   padding: 0;
   margin: 0;
+  border: 1px solid #cccccc;
 `;
 
 export default FieldDropDown;
