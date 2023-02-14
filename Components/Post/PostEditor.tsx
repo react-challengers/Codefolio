ce747235bca1c0545c3cf33f5465248f25e148d3
hyoloui <tmdgy9272@gmail.com>
2023-02-14 18:00:55 +0900

    feat: (PostEditor) colorSyntax 적용
    - colorSyntax설치
    
    #5 #41

--- Components/Post/PostEditor.tsx
+++ Components/Post/PostEditor.tsx
@@ -1,11 +1,9 @@
-// import "@toast-ui/editor/dist/toastui-editor.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
 // import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 // import "tui-color-picker/dist/tui-color-picker.css";
 // import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import { Editor } from "@toast-ui/react-editor";
-// import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-
-require("@toast-ui/editor/dist/toastui-editor.css");
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
 
 const PostEditor = () => {
   const toolbarItems = [
@@ -22,8 +20,16 @@ const PostEditor = () => {
       initialValue="hello react editor world!"
       previewStyle="vertical"
       height="600px"
-      initialEditType="markdown"
-      useCommandShortcut={true}
+      initialEditType="wysiwyg"
+      useCommandShortcut
+      toolbarItems={toolbarItems}
+      plugins={[
+        colorSyntax,
+        // 기본 색상 preset 적용
+        // {
+        //   preset: ["#1F2E3D", "#4c5864", "#ED7675"],
+        // },
+      ]}
     />
   );
 };
