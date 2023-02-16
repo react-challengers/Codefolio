87f07c65680223b971ee2b7f3dc5ff52350e582e
nno3onn <nno3onn@naver.com>
2023-02-16 11:27:37 +0900

    feat(Bookmark): 디테일 페이지 북마크 기능 구현

--- Components/Detail/DetailHeader.tsx
+++ Components/Detail/DetailHeader.tsx
@@ -1,22 +1,80 @@
+import supabase from "@/lib/supabase";
+import { User } from "@supabase/supabase-js";
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import ShowMoreModal from "./ShowMoreModal";
 
 const DetailHeader = () => {
+  const {
+    query: { id: postId }, // c078f3bf-4e86-44a2-a672-583f36c1aa8f
+  } = useRouter();
   const [showMore, setShowMore] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const [isBookmark, setIsBookmark] = useState(false);
 
   const showMoreModal = () => setShowMore((prev) => !prev);
+
+  const getUser = async () => {
+    const { data } = await supabase.auth.getUser();
+    setUser(data.user);
+  };
+
+  const getBookmark = async () => {
+    const { data } = await supabase
+      .from("bookmark")
+      .select()
+      .eq("user_id", user?.id)
+      .eq("post_id", postId)
+      .single();
+    setIsBookmark(!!data?.data);
+  };
+
+  const addBookmark = async () => {
+    const { error } = await supabase
+      .from("bookmark")
+      .insert({ post_id: postId, user_id: user?.id });
+    if (!error) {
+      setIsBookmark(true);
+    }
+  };
+
+  const deleteBookmark = async () => {
+    const { error } = await supabase
+      .from("bookmark")
+      .delete()
+      .eq("user_id", user?.id)
+      .eq("post_id", postId);
+    if (!error) {
+      setIsBookmark(false);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+    getBookmark();
+  }, []);
+
+  const clickBookmarkButton = async () => {
+    if (isBookmark) {
+      deleteBookmark();
+    } else {
+      addBookmark();
+    }
+  };
 
   return (
     <DetailHeaderContainer>
       <DetailHeaderWrapper>
         <Image src="/icons/like.svg" width={36} height={36} alt="좋아요 버튼" />
-        <Image
+        <BookmarkButton
           src="/icons/bookmark.svg"
           width={36}
           height={36}
           alt="북마크 버튼"
+          onClick={clickBookmarkButton}
+          isBookmark={isBookmark}
         />
         <Image
           src="/icons/more.svg"
@@ -36,6 +94,14 @@ const DetailHeaderContainer = styled.div`
   position: relative;
   img {
     cursor: pointer;
+  }
+`;
+
+const BookmarkButton = styled(Image)<{ isBookmark: boolean }>`
+  background-color: ${({ isBookmark }) =>
+    isBookmark ? "yellow" : "transparent"};
+  &:hover {
+    background-color: yellow;
   }
 `;
 
