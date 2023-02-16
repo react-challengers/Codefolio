d90b39b0987f18677fcdd65180bfeb9d4b7346e4
Jeremy-kr <wjddlr905@gmail.com>
2023-02-16 11:01:37 +0900

    fix(next.config.js): images 설정을 추가합니다.
    - 외부 이미지를 불러오기 위해 next.config.js 파일을 수정합니다.
    
    #50

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["xxfgrnzupwpguxifhwsq.supabase.co"],
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
