f5555e63a87153285e679d2318c50888d6d151c9
hyoloui <tmdgy9272@gmail.com>
2023-02-21 18:20:54 +0900

    fix(PostEditor): 뷰어에 설치
    - 뷰어에 plugins 속성으로 추가하였습니다
    
    #167

--- Components/Detail/DetailContent.tsx
+++ Components/Detail/DetailContent.tsx
@@ -1,12 +1,22 @@
-import { Viewer } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+import "prismjs/themes/prism.css";
+import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+
+import { Viewer } from "@toast-ui/react-editor";
+import codeSyntaxHighlightPlugin from "@toast-ui/editor-plugin-code-syntax-highlight";
+import Prism from "prismjs";
 
 interface DetailContentProps {
   content: string;
 }
 
 const DetailContent = ({ content }: DetailContentProps) => {
-  return <Viewer initialValue={content} />;
+  return (
+    <Viewer
+      initialValue={content}
+      plugins={[[codeSyntaxHighlightPlugin, { highlighter: Prism }]]}
+    />
+  );
 };
 
 export default DetailContent;
