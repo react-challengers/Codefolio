6758ff56a4a6aaa7d98cf4ea3e1723112d1c817b
Jeremy-kr <wjddlr905@gmail.com>
2023-02-22 16:36:32 +0900

    chore: 마이페이지 폴더 리팩토링

--- Components/MyPage/UserInfoContainer/UserInfoContainer.tsx
+++ Components/MyPage/UserInfoContainer/UserInfoContainer.tsx
@@ -7,7 +7,6 @@ import { useUserProfile } from "@/hooks/query";
 import { useInput } from "@/hooks/common";
 import { ProfileImage } from "@/Components/Common";
 import Banner from "./Banner";
-import color_fill from "../../public/icons/color_fill.svg";
 
 /**
  * @TODO SelfProfileWrapper 최대 3줄로 제한하기
@@ -121,7 +120,7 @@ const UserInfoContainer = () => {
               />
               <ImgLabel htmlFor="background-color-picker">
                 <ImgIcon
-                  src={color_fill}
+                  src="/icons/color_fill.svg"
                   alt="배경색 지정 아이콘"
                   width={36}
                   height={36}
