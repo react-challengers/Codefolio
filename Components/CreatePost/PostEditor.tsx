158e3bd38495b4a28b463581d78607cf5061b173
hyoloui <tmdgy9272@gmail.com>
2023-02-27 12:31:14 +0900

    delete(toastUI): 기존 에디터 삭제
    editor 변경 #224 @hyoloui

--- Components/CreatePost/PostEditor.tsx
+++ Components/CreatePost/PostEditor.tsx
@@ -1,16 +1,7 @@
-import "@toast-ui/editor/dist/toastui-editor.css";
-import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
-import "tui-color-picker/dist/tui-color-picker.css";
-import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
-import "@toast-ui/editor/dist/i18n/ko-kr";
-import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
-import "prismjs/themes/prism.css";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
 
-import { Editor } from "@toast-ui/react-editor";
-import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-import codeSyntaxHighlightPlugin from "@toast-ui/editor-plugin-code-syntax-highlight";
-import Prism from "prismjs";
-import { RefObject, useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import supabase from "@/lib/supabase";
 import imageCompression from "browser-image-compression";
 import { useRecoilState } from "recoil";
@@ -21,21 +12,8 @@ import { postContent as recoilPostContent } from "@/lib/recoil";
  * @TODO uuid flag 꽃아야 함 >> 게시와 임시저장의 용도로 분류
  */
 
-interface PostEditorProps {
-  editorRef: RefObject<Editor>;
-}
-
-const PostEditor = ({ editorRef }: PostEditorProps) => {
+const PostEditor = () => {
   const [postContent, setPostContent] = useRecoilState(recoilPostContent);
-  const toolbarItems = [
-    ["heading", "bold", "italic", "strike"],
-    ["hr"],
-    ["ul", "ol", "task"],
-    ["table", "link"],
-    ["image"], // <-- 이미지 추가 툴바
-    ["code"],
-    ["scrollSync"],
-  ];
 
   // 이미지 추가
   type HookCallback = (url: string, text?: string) => void;
@@ -48,16 +26,7 @@ const PostEditor = ({ editorRef }: PostEditorProps) => {
     dropImage(url, `${blob.name}`); // 에디터에 이미지 추가
   }, []);
 
-  useEffect(() => {
-    if (editorRef.current) {
-      const editorIns = editorRef.current.getInstance();
-      editorIns.removeHook("addImageBlobHook");
-      editorIns.addHook("addImageBlobHook", addImage);
-    }
-  }, [editorRef, addImage]);
-
   // 이미지 업로드
-
   const uploadImage = async (blob: File) => {
     try {
       const imgPath = crypto.randomUUID();
@@ -86,37 +55,17 @@ const PostEditor = ({ editorRef }: PostEditorProps) => {
     return result;
   };
 
-  const handleOnEditorChange = () => {
-    // 유효성 검사
-    const editorText = editorRef.current?.getInstance().getMarkdown();
-    if (editorText === " " || editorText === "" || editorText === undefined) {
-      return;
-    }
-    // HTML 대신에 Markdown으로 저장합니다.
-    setPostContent(editorText);
-  };
+  // const handleOnEditorChange = () => {
+  //   // 유효성 검사
+  //   const editorText = editorRef.current?.getInstance().getMarkdown();
+  //   if (editorText === " " || editorText === "" || editorText === undefined) {
+  //     return;
+  //   }
+  //   // HTML 대신에 Markdown으로 저장합니다.
+  //   setPostContent(editorText);
+  // };
 
-  return (
-    <Editor
-      ref={editorRef}
-      initialValue={postContent ?? null}
-      previewStyle="vertical"
-      height="600px"
-      initialEditType="markdown"
-      useCommandShortcut
-      toolbarItems={toolbarItems}
-      language="ko-KR"
-      plugins={[
-        colorSyntax,
-        [codeSyntaxHighlightPlugin, { highlighter: Prism }],
-      ]}
-      hooks={{
-        // @ts-ignore
-        addImageBlobHook: addImage,
-      }}
-      onChange={() => handleOnEditorChange()}
-    />
-  );
+  return <div>에디터</div>;
 };
 
 export default PostEditor;
