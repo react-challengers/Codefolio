015848a4e0fe0e4d24409b1b62c6c628da5fb17d
hyoloui <tmdgy9272@gmail.com>
2023-02-28 17:52:47 +0900

    feat(PostEditor): commands toolbar 수정
    - 툴바를 커스텀하여 이미지를 업로드 합니다
    
    editor 변경 #224 @hyoloui @nno3onn

--- Components/CreatePost/PostEditor.tsx
+++ Components/CreatePost/PostEditor.tsx
@@ -8,35 +8,58 @@ import { v4 as uuidv4 } from "uuid";
 import { useRecoilState } from "recoil";
 import { postContent as recoilPostContent, postId } from "@/lib/recoil";
 import imageCompression from "browser-image-compression";
+import { MDEditorProps } from "@uiw/react-md-editor";
+import { NextPage } from "next";
+import * as commands from "@uiw/react-md-editor/lib/commands";
 
 /**
- * @TODO postId 에러 해결 필요
  * @TODO 이미지 업로드시 링크 열리는 문제 해결 필요
- * @TODO 이미지 아이콘 클릭시 이미지 업로드 구현 필요
  * @TODO storage 삭제 구현 필요
  */
 
-const MDEditor = dynamic(() => import("@uiw/react-md-editor"), {
+const MDEditor = dynamic<MDEditorProps>(() => import("@uiw/react-md-editor"), {
   ssr: false,
 });
 
-const PostEditor = () => {
+const PostEditor: NextPage = () => {
   const [isPostId] = useRecoilState(postId);
   const [postContent, setPostContent] = useRecoilState(recoilPostContent);
 
   const onImagePasted = useCallback(
     async (
-      dataTransfer: DataTransfer // Drag and Drop API
+      dataTransfer: DataTransfer | any // Drag and Drop API
     ) => {
       const files: File[] = []; // 드래그 앤 드랍으로 가져온 파일들
-      for (let index = 0; index < dataTransfer.items.length; index += 1) {
-        const file = dataTransfer.files.item(index);
+      if (dataTransfer.items) {
+        for (let index = 0; index < dataTransfer.items.length; index += 1) {
+          const file = dataTransfer.items[index].getAsFile();
+          if (!file) return;
+          files.push(file);
+        }
+      } else {
+        const file = dataTransfer[0];
+        console.log("👉👉  file:", file);
+
         if (!file) return;
         files.push(file);
       }
+
+      // for (
+      //   let index = 0;
+      //   index <
+      //   (dataTransfer.items ? dataTransfer.items.length : dataTransfer.length);
+      //   index += 1
+      // ) {
+      //   const file = dataTransfer.items
+      //     ? dataTransfer.files.item(index)
+      //     : dataTransfer[0];
+      //   if (!file) return;
+      //   files.push(file);
+      // }
       const fileId = uuidv4();
       files.map(async (file) => {
         const compressedFile = await compressImg(file);
+
         if (!compressedFile) return;
 
         const { data: uploadImg } = await supabase.storage
@@ -109,6 +132,76 @@ const PostEditor = () => {
         textareaProps={{
           placeholder: "Fill in your markdown for the coolest of the cool.",
         }}
+        commands={[
+          commands.bold,
+          commands.italic,
+          commands.strikethrough,
+          commands.hr,
+          commands.title,
+          commands.divider,
+
+          commands.link,
+          commands.group([], {
+            name: "image",
+            groupName: "image",
+            icon: (
+              <svg
+                fill="#444541"
+                height="12"
+                width="12"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 489.4 489.4"
+              >
+                <g>
+                  <g>
+                    <path
+                      d="M0,437.8c0,28.5,23.2,51.6,51.6,51.6h386.2c28.5,0,51.6-23.2,51.6-51.6V51.6c0-28.5-23.2-51.6-51.6-51.6H51.6
+			C23.1,0,0,23.2,0,51.6C0,51.6,0,437.8,0,437.8z M437.8,464.9H51.6c-14.9,0-27.1-12.2-27.1-27.1v-64.5l92.8-92.8l79.3,79.3
+			c4.8,4.8,12.5,4.8,17.3,0l143.2-143.2l107.8,107.8v113.4C464.9,452.7,452.7,464.9,437.8,464.9z M51.6,24.5h386.2
+			c14.9,0,27.1,12.2,27.1,27.1v238.1l-99.2-99.1c-4.8-4.8-12.5-4.8-17.3,0L205.2,333.8l-79.3-79.3c-4.8-4.8-12.5-4.8-17.3,0
+			l-84.1,84.1v-287C24.5,36.7,36.7,24.5,51.6,24.5z"
+                    />
+                    <path
+                      d="M151.7,196.1c34.4,0,62.3-28,62.3-62.3s-28-62.3-62.3-62.3s-62.3,28-62.3,62.3S117.3,196.1,151.7,196.1z M151.7,96
+			c20.9,0,37.8,17,37.8,37.8s-17,37.8-37.8,37.8s-37.8-17-37.8-37.8S130.8,96,151.7,96z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            ),
+            // eslint-disable-next-line react/no-unstable-nested-components
+            children: (handle: any) => {
+              return (
+                <div style={{ width: 200, padding: 10 }}>
+                  <input
+                    type="file"
+                    onChange={(e) => onImagePasted(e.target.files)}
+                  />
+                  <button type="button" onClick={() => handle.close()}>
+                    close
+                  </button>
+                </div>
+              );
+            },
+            execute: (
+              state: commands.TextState,
+              api: commands.TextAreaTextApi
+            ) => {
+              console.log(">>>>>>update>>>>>", state);
+            },
+            buttonProps: { "aria-label": "Insert title" },
+          }),
+          commands.quote,
+          commands.code,
+          commands.divider,
+
+          commands.unorderedListCommand,
+          commands.orderedListCommand,
+          commands.checkedListCommand,
+          commands.divider,
+        ]}
       />
     </div>
   );
