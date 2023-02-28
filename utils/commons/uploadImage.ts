7f3374711411f8dae1a277c0db8d6ca279ed53c7
hyoloui <tmdgy9272@gmail.com>
2023-03-01 05:17:33 +0900

    refactor(utill): 업로드이미지 유틸 수정, 압축이미지 유틸 추가
    - 이미지 압축 함수를 유틸화 합니다.
    - 기존의 uploadImage 함수를 부분 수정합니다.(폴더링 가능)
    
    #224

--- utils/commons/uploadImage.ts
+++ utils/commons/uploadImage.ts
@@ -1,15 +1,21 @@
 import supabase from "@/lib/supabase";
+import { v4 as uuidv4 } from "uuid";
 
 type StorageName = "post-image" | "profile-image" | "background-image";
+type IsFolder = string;
 
-const uploadImage = async (file: File, storageName: StorageName) => {
-  const imgPath = crypto.randomUUID();
+const uploadImage = async (
+  file: File,
+  storageName: StorageName,
+  foldering?: IsFolder
+) => {
+  const imgPath = uuidv4();
   try {
-    await supabase.storage.from(storageName).upload(imgPath, file);
+    await supabase.storage.from(storageName).upload(foldering || imgPath, file);
 
     const { data } = await supabase.storage
       .from(storageName)
-      .getPublicUrl(imgPath);
+      .getPublicUrl(foldering || imgPath);
     return data.publicUrl;
   } catch (error) {
     return "";
