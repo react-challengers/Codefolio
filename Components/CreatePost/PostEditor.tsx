6ac7503a72c1b493b98fde456e6c68339ddbef0e
hyoloui <tmdgy9272@gmail.com>
2023-03-01 05:22:29 +0900

    refactor(PostEditor): 포스트에디터 리펙토링
    - 커스텀 툴바 (이미지) 스타일 추가
    - 유효성 검사 적용
    - 여러 파일 업로드 기능 적용
    - 드래그앤드랍 후 새탭 열리지 않게 적용
    
    #224 @hyoloui

--- Components/CreatePost/PostEditor.tsx
+++ Components/CreatePost/PostEditor.tsx
@@ -3,19 +3,19 @@ import "@uiw/react-markdown-preview/markdown.css";
 
 import { useCallback } from "react";
 import dynamic from "next/dynamic";
-import supabase from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { postContent as recoilPostContent, postId } from "@/lib/recoil";
-import imageCompression from "browser-image-compression";
 import type { MDEditorProps } from "@uiw/react-md-editor";
 import { NextPage } from "next";
 import * as commands from "@uiw/react-md-editor/lib/commands";
 import uploadImage from "@/utils/commons/uploadImage";
+import styled from "styled-components";
+import compressImg from "@/utils/commons/compressImg";
+import validateFile from "@/utils/commons/validationImage";
 
 /**
- * @TODO supabase api utill함수로 사용하도록 변경 필요
- * @TODO 이미지 업로드시 링크 열리는 문제 해결 필요
+ * @TODO 이미지 확장자 유효성 검사 필요
  * @TODO storage 삭제 구현 필요
  */
 
@@ -31,37 +31,44 @@ const PostEditor: NextPage = () => {
     async (
       dataTransfer: DataTransfer | any // Drag and Drop API
     ) => {
-      const files: File[] = []; // 드래그 앤 드랍으로 가져온 파일들
+      const files: File[] = [];
+
       if (dataTransfer.items) {
         for (let index = 0; index < dataTransfer.items.length; index += 1) {
           const file = dataTransfer.items[index].getAsFile();
-          if (!file) return;
+
+          if (!validateFile(file)) {
+            console.log("이미지 파일이 아닙니다.");
+            return;
+          }
           files.push(file);
         }
       } else {
-        const file = dataTransfer[0];
+        for (let index = 0; index < dataTransfer.length; index += 1) {
+          const file = dataTransfer[index];
 
-        if (!file) return;
-        files.push(file);
+          if (!validateFile(file)) {
+            console.log("이미지 파일이 아닙니다.");
+            return;
+          }
+          files.push(file);
+        }
       }
 
-      const fileId = uuidv4();
       files.map(async (file) => {
-        const compressedFile = await compressImg(file);
+        const fileId = uuidv4() + file.name;
 
+        const compressedFile = await compressImg(file);
         if (!compressedFile) return;
 
-        const { data: uploadImg } = await supabase.storage
-          .from("post-image")
-          .upload(`${isPostId}/${fileId}`, compressedFile);
-        if (!uploadImg) return;
+        const uploadResult = await uploadImage(
+          compressedFile,
+          "post-image",
+          `${isPostId}/${fileId}`
+        );
+        if (!uploadResult) return;
 
-        const { data: insertedMarkdown } = supabase.storage
-          .from("post-image")
-          .getPublicUrl(`${isPostId}/${fileId}`);
-        if (!insertedMarkdown) return;
-
-        const insertString = `![${file.name}](${insertedMarkdown.publicUrl})`;
+        const insertString = `![${file.name}](${uploadResult})`;
         const resultString = insertToTextArea(insertString);
 
         setPostContent(resultString || "");
@@ -69,18 +76,6 @@ const PostEditor: NextPage = () => {
     },
     [isPostId, setPostContent]
   );
-
-  // 이미지 압축
-  const compressImg = async (blob: File): Promise<File | void> => {
-    const options = {
-      maxSizeMB: 1,
-      initialQuality: 0.55, // initial 0.7
-    };
-    const result = await imageCompression(blob, options)
-      .then((res) => res)
-      .catch((e) => console.log(e, "압축 에러"));
-    return result;
-  };
 
   // 에디터에 이미지 추가
   const insertToTextArea = (intsertString: string) => {
@@ -104,7 +99,6 @@ const PostEditor: NextPage = () => {
   };
 
   return (
-    // div에 클래스를 적용하여 다크모드를 수동으로 적용할 수 있습니다.
     <MDEditor
       value={postContent}
       onChange={(value) => {
@@ -112,9 +106,11 @@ const PostEditor: NextPage = () => {
       }}
       height={600}
       onPaste={(event) => {
+        event.preventDefault();
         onImagePasted(event.clipboardData);
       }}
       onDrop={(event) => {
+        event.preventDefault();
         onImagePasted(event.dataTransfer);
       }}
       textareaProps={{
@@ -129,53 +125,6 @@ const PostEditor: NextPage = () => {
         commands.divider,
 
         commands.link,
-        commands.group([], {
-          name: "image",
-          groupName: "image",
-          icon: (
-            <svg
-              fill="#444541"
-              height="12"
-              width="12"
-              version="1.1"
-              id="Capa_1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 489.4 489.4"
-            >
-              <g>
-                <g>
-                  <path
-                    d="M0,437.8c0,28.5,23.2,51.6,51.6,51.6h386.2c28.5,0,51.6-23.2,51.6-51.6V51.6c0-28.5-23.2-51.6-51.6-51.6H51.6
-			C23.1,0,0,23.2,0,51.6C0,51.6,0,437.8,0,437.8z M437.8,464.9H51.6c-14.9,0-27.1-12.2-27.1-27.1v-64.5l92.8-92.8l79.3,79.3
-			c4.8,4.8,12.5,4.8,17.3,0l143.2-143.2l107.8,107.8v113.4C464.9,452.7,452.7,464.9,437.8,464.9z M51.6,24.5h386.2
-			c14.9,0,27.1,12.2,27.1,27.1v238.1l-99.2-99.1c-4.8-4.8-12.5-4.8-17.3,0L205.2,333.8l-79.3-79.3c-4.8-4.8-12.5-4.8-17.3,0
-			l-84.1,84.1v-287C24.5,36.7,36.7,24.5,51.6,24.5z"
-                  />
-                  <path
-                    d="M151.7,196.1c34.4,0,62.3-28,62.3-62.3s-28-62.3-62.3-62.3s-62.3,28-62.3,62.3S117.3,196.1,151.7,196.1z M151.7,96
-			c20.9,0,37.8,17,37.8,37.8s-17,37.8-37.8,37.8s-37.8-17-37.8-37.8S130.8,96,151.7,96z"
-                  />
-                </g>
-              </g>
-            </svg>
-          ),
-          // eslint-disable-next-line react/no-unstable-nested-components
-          children: (handle: any) => {
-            return (
-              <div style={{ width: 200, padding: 10 }}>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={(e) => onImagePasted(e.target.files)}
-                />
-                <button type="button" onClick={() => handle.close()}>
-                  close
-                </button>
-              </div>
-            );
-          },
-          buttonProps: { "aria-label": "Insert image" },
-        }),
         commands.quote,
         commands.code,
         commands.divider,
@@ -184,9 +133,85 @@ const PostEditor: NextPage = () => {
         commands.orderedListCommand,
         commands.checkedListCommand,
         commands.divider,
+
+        commands.group([], {
+          name: "image",
+          groupName: "image",
+          icon: (
+            <svg width="12" height="12" viewBox="0 0 20 20">
+              <path
+                fill="currentColor"
+                d="M15 9c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm4-7H1c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h18c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 13l-6-5-2 2-4-5-4 8V4h16v11z"
+              />
+            </svg>
+          ),
+          // eslint-disable-next-line react/no-unstable-nested-components
+          children: (handle) => {
+            return (
+              <CustomImageContainer>
+                <label htmlFor="file">
+                  <ImageUploadButton>이미지 업로드하기</ImageUploadButton>
+                </label>
+                <ImageInput
+                  type="file"
+                  id="file"
+                  value=""
+                  multiple
+                  accept="image/*"
+                  onChange={(e) => {
+                    onImagePasted(e.target.files);
+                    handle.close();
+                  }}
+                />
+                <CloseButton type="button" onClick={() => handle.close()}>
+                  close
+                </CloseButton>
+              </CustomImageContainer>
+            );
+          },
+          buttonProps: { "aria-label": "Insert image" },
+        }),
       ]}
     />
   );
 };
 
 export default PostEditor;
+
+const CustomImageContainer = styled.div`
+  width: 15rem;
+  padding: 0.625rem;
+  background-color: #eee;
+`;
+
+const ImageUploadButton = styled.div`
+  width: 9.375rem;
+  height: 1.875rem;
+  background: #fff;
+  border: 1px solid rgb(77, 77, 77);
+  border-radius: 0.625rem;
+  font-weight: 500;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    background: rgb(77, 77, 77);
+    color: #fff;
+  }
+`;
+
+const ImageInput = styled.input`
+  display: none;
+`;
+
+const CloseButton = styled.button`
+  all: unset;
+  display: block;
+  margin-left: auto;
+  margin-top: 0.625rem;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
