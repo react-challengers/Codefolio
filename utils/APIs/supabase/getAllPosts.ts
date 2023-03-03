b8032f5e1883bb3a30c8af9664755b622fea9bf7
arch-spatula <shkim516@naver.com>
2023-03-03 19:49:24 +0900

    doc(getAllPosts): 더욱더 정확한 타입지정
    #243

--- utils/APIs/supabase/getAllPosts.ts
+++ utils/APIs/supabase/getAllPosts.ts
@@ -4,9 +4,8 @@ import { PostgrestSingleResponse } from "@supabase/supabase-js";
 // TODO: Add pagination
 // TODO: 필요한 데이터만 가져오도록 수정
 
-//
 const getAllPosts = async () => {
-  const res: PostgrestSingleResponse<PostType[]> = await supabase
+  const res = await supabase
     .from("post")
     .select<string, PostType>("*")
     .order("created_at", { ascending: false });
