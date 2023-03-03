9609d41d8c5318641040403e305ba952a8729cfa
nno3onn <nno3onn@naver.com>
2023-03-03 15:41:21 +0900

    feat: 로그인 모달 구현

---
+++ Components/Detail/LoginModal.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+
+interface DeleteModalProps {
+  onCancel: () => void;
+  onLogin: () => void;
+}
+
+const LoginModal = ({ onCancel, onLogin }: DeleteModalProps) => {
+  return (
+    <DeleteModalContainer>
+      <ModalContainer>
+        <ModalBody>로그인하시겠습니까?</ModalBody>
+        <ModalFooter>
+          <CancelButton onClick={onCancel}>취소</CancelButton>
+          <DeleteButton onClick={onLogin}>로그인</DeleteButton>
+        </ModalFooter>
+      </ModalContainer>
+    </DeleteModalContainer>
+  );
+};
+
+const DeleteModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContainer = styled.div`
+  box-shadow: 0px 0.25rem 0.625rem rgba(0, 0, 0, 0.25);
+  border-radius: 0.25rem;
+  width: 20.5rem;
+  background-color: ${({ theme }) => theme.colors.gray11};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalBody = styled.div`
+  padding: 4.0625rem 0;
+  color: white;
+`;
+
+const ModalFooter = styled.div`
+  display: flex;
+  width: 100%;
+  height: 3.75rem;
+`;
+
+const Button = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+`;
+
+const CancelButton = styled(Button)`
+  color: white;
+  background-color: ${({ theme }) => theme.colors.gray4};
+  border-bottom-left-radius: 4px;
+`;
+
+const DeleteButton = styled(Button)`
+  color: black;
+  background-color: ${({ theme }) => theme.colors.primary4};
+  border-bottom-right-radius: 4px;
+`;
+
+export default LoginModal;
