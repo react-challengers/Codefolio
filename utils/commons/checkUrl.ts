80accb9d2bdb40268a08584936e4238aed882f66
nno3onn <nno3onn@naver.com>
2023-03-08 14:22:44 +0900

    refac: 깃허브 주소 유효성 검사

--- utils/commons/checkUrl.ts
+++ utils/commons/checkUrl.ts
@@ -1,6 +1,6 @@
 // url 유효성 검사 함수
 const checkUrl = (url: string): boolean => {
-  const expression = /^(ftp|http|https):\/\/[^ "]+$/;
+  const expression = /^(ftp|http|https):\/\/github.com\/[^ "]+$/;
   const regex = new RegExp(expression);
 
   return regex.test(url);
